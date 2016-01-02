b795aac88d08bd439f90b0c3606cc5360740afd6
Ricardo Ferreira <ricardomfferreira@gmail.com>
2016-01-02 18:55:16 +0000

    Added Zombie and fine tuned animation

---
+++ Assets/Scripts/MyAICharacterControl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof (NavMeshAgent))]
+	[RequireComponent(typeof (MyThirdPersonController))]
+	public class MyAICharacterControl : MonoBehaviour
+    {
+        public NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
+		public MyThirdPersonController character { get; private set; } // the character we are controlling
+        public Transform target;                                    // target to aim for
+
+
+        private void Start()
+        {
+            // get the components on the object we need ( should not be null due to require component so no need to check )
+            agent = GetComponentInChildren<NavMeshAgent>();
+			character = GetComponent<MyThirdPersonController>();
+
+	        agent.updateRotation = false;
+	        agent.updatePosition = true;
+        }
+
+
+        private void Update()
+        {
+            if (target != null)
+                agent.SetDestination(target.position);
+
+            if (agent.remainingDistance > agent.stoppingDistance)
+                character.Move(agent.desiredVelocity);
+            else
+                character.Move(Vector3.zero);
+        }
+
+
+        public void SetTarget(Transform target)
+        {
+            this.target = target;
+        }
+    }
+}
+
