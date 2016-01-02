e4920d9a40a7648d89b02e5e000abf60237a8e8c
Ricardo Ferreira <ricardomfferreira@gmail.com>
2016-01-02 17:41:06 +0000

    Added Helicopter. Fine tuned sounds.

---
+++ Assets/Scripts/Helicopter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Helicopter : MonoBehaviour {
+
+	public AudioClip callSound;
+
+	private bool called = false;
+	private AudioSource audioSource;
+
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("CallHeli") && !called) {
+			called = true;
+			audioSource.clip = callSound;
+			audioSource.Play();
+			Debug.Log ("Helicopter called");
+		}
+	}
+}
