8fd868d9514cafb6a45c1ae1f89d7267eaf0f1c5
Ricardo Ferreira <ricardomfferreira@gmail.com>
2016-01-21 21:45:24 +0000

    Now there's a winning screen with an helicopter sound.

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -9,10 +9,20 @@ public class GameOverScreen : MonoBehaviour {
 	void Start () {
 		audioSource = GetComponent<AudioSource>();
 		audioSource.Play();
+		if (SceneManager.GetActiveScene().buildIndex == 3) {
+			StartCoroutine(FadeOutSound());
+		}
 		Invoke("MainMenu", 5f);
 	}
 
 	void MainMenu () {
 		SceneManager.LoadScene(0);
+	}
+
+	IEnumerator FadeOutSound () {
+		for (float intensity = 0.5f; intensity >= 0f; intensity -= 0.002f) {
+			audioSource.volume = intensity;
+			yield return null;
+		}
 	}
 }
