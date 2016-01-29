da6170bd52e90bf60b3ebe80e9bb89e6ea931fd2
Ricardo Ferreira <ricardomfferreira@gmail.com>
2016-01-29 21:13:03 +0000

    Added Controls Menu to Start Screen

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -6,14 +6,18 @@ using System.Collections;
 public class StartMenu : MonoBehaviour {
 
 	public Canvas quitMenu;
+	public Canvas controlsMenu;
 	public Button startText;
+	public Button controlsText;
 	public Button exitText;
 
  	void Start () {
  		quitMenu = quitMenu.GetComponent<Canvas>();
 		startText = startText.GetComponent<Button>();
+		controlsText = controlsText.GetComponent<Button>();
 		exitText = exitText.GetComponent<Button>();
 		quitMenu.enabled = false;
+		controlsMenu.enabled = false;
 		Cursor.visible = true;
 		Cursor.lockState = CursorLockMode.None;
  	}
@@ -22,12 +26,22 @@ public class StartMenu : MonoBehaviour {
  		quitMenu.enabled = true;
  		startText.enabled = false;
  		exitText.enabled = false;
+ 		controlsText.enabled = false;
+ 	}
+
+	public void ControlsPress () {
+		controlsMenu.enabled = true;
+ 		startText.enabled = false;
+ 		exitText.enabled = false;
+		controlsText.enabled = false;
  	}
 
 	public void NoPress () {
  		quitMenu.enabled = false;
+		controlsMenu.enabled = false;
  		startText.enabled = true;
  		exitText.enabled = true;
+ 		controlsText.enabled = true;
  	}
 
  	public void StartLevel () {
